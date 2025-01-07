237c86db7a523feb04d51a36917e81a8747c06e4
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2025-01-07 19:07:32 +0200

    Delete compare_deltas func, add is_possible_to_collect_series func

--- player_model.py
+++ player_model.py
@@ -11,7 +11,9 @@ class Player:
         self.money: int = money
         self.cards: List[int] = cards
         self._score: int = -11
-        self._delta: int = 0
+
+        self._deltas: int = []
+        self._min_delta: int = 0
 
 
     @classmethod
@@ -27,29 +29,36 @@ class Player:
 
 
     @property
-    def calculate_delta(self) -> int:
-        return self._delta
+    def min_delta(self) -> int:
+        self._min_delta = min(self.calculate_deltas, key=abs) if len(self.calculate_deltas) != 0 else 0
+        return self._min_delta
 
 
-    @calculate_delta.setter
-    def calculate_delta(self, table_card: int) -> int:
-        """
-        Calculate the difference between the current card on the table and the card closest to it in the player's hand.
-        The difference is calculated as the absolute difference between the two cards. If the player has multiple cards in their hand with the same difference to the table card, the difference of the first card in the first group of cards is used. If the player has no cards in their hand, the difference is set to 0.
-        """
-        self._delta = 0
+    @property
+    def calculate_deltas(self) -> int:
+        return self._deltas
+
+
+    @calculate_deltas.setter
+    def calculate_deltas(self, table_card: int) -> int:
+        _deltas = []
+        self._deltas = []
         if len(self.cards) != 0:
             for group_of_cards in self.cards:
+                _delta = 0
                 for card in [group_of_cards[0], group_of_cards[-1]]:
                     card_difference = table_card - card
-                    if abs(card_difference) < abs(self._delta) or self._delta == 0:
-                        self._delta = card_difference
+                    if abs(card_difference) < abs(_delta) or _delta == 0:
+                        _delta = card_difference
+                _deltas.append(_delta)
+            self._deltas = _deltas          
 
 
     def update_state(self, money: int, cards: List[int], table_card: int):
         self.money = money
         self.cards = cards
-        self.calculate_delta = table_card
+        self.calculate_deltas = table_card
+        self.min_delta
 
 
 class BotPlayer(Player):
@@ -59,43 +68,42 @@ class BotPlayer(Player):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-
-    def compare_deltas(self, players_deltas: List[int]) -> bool:
-        """The function compares players deltas and the bot delta.
-        Returns False if one of the players' delta is closer to zero.
-        """
-        if self.calculate_delta == 0:
-            return False
-        
-        for player_delta in players_deltas:
-            if (self.calculate_delta > 0 and player_delta > 0 and self.calculate_delta > player_delta) or (self.calculate_delta < 0 and player_delta < 0 and self.calculate_delta < player_delta):
-                return False
-            
+    
+    def is_possible_to_collect_series(self, players: List[dict]) -> bool:
+        if self.calculate_deltas:
+            botPlayer_min_delta = self.min_delta
+            for player in players:
+                for delta in player.calculate_deltas:
+                    if (delta > 0 and botPlayer_min_delta > 0 and delta < botPlayer_min_delta) or (delta < 0 and botPlayer_min_delta < 0 and delta > botPlayer_min_delta):
+                        return False # not possible to collect series
         return True
 
 
-    def decide(self, table_card: int, table_money: int, cards_left: int, players_deltas: List[int], players_money: List[int]) -> bool:
+    def decide(self, table_card: int, table_money: int, cards_left: int, players: List[dict]) -> bool:
+        is_possible_to_collect_series = self.is_possible_to_collect_series(players)
+        print(f"is_possible_to_collect_series: {is_possible_to_collect_series}")
 
-        # Check if BotPlayer has cards [16, 17] and on the table card 19 (delta = 2) and someone has card 18 (delta = 1), then BotPlayer should bet
-        check_if_bot_delta_less_then_other_players_delta = self.compare_deltas(players_deltas)
+        players_min_delatas = [player.min_delta for player in players]
+        players_money = [player.money for player in players]
 
         # if it's the first move in the game
         if cards_left == 24 and table_card <= 24 and table_money > 3:
-            print("If first table card is less than 24 and table money is more than 1")
+            print("If first table card is less or equal than 24 and table money is more than 1")
             return True
         
-        # if it's the first move in the game and card is less or equal 10
+        # if it's the first move in the game and card is less or equal 15 and table money is more than 1
         if cards_left == 24 and table_card <= 15 and table_money > 1:
-            print("If first card is less than 10")
+            print("If first card is less than 15 and table money is more than 1")
             return True
 
         # if the card is important for the BotPlayer and one of other player
-        if abs(self.calculate_delta) == 1 and (1 in list(map(abs, players_deltas)) or 0 in players_money):
-            print("If card important for BotPlayer and one of other player, or someone has no money")
+        if abs(self.min_delta) == 1 and (1 in list(map(abs, players_min_delatas)) or 0 in players_money):
+            print(self.min_delta, players_min_delatas)
+            print("If card important for BotPlayer and one of other player, or one of opponent has no money")
             return True
         
         # Bet if there is no money on the table.
-        if (table_money < 2 and self.money > 0 and table_card > 10 and self.calculate_delta != -1): #  and cards_left > 8
+        if (table_money < 2 and self.money > 0 and table_card > 10 and self.min_delta != -1): #  and cards_left > 8
             print("Place a bet")
             return False
         
@@ -105,17 +113,20 @@ class BotPlayer(Player):
             return True
 
         # Take a card if the card is in the range calculated by the formula (cards_left//8)+1
-        if cards_left < 24 and abs(self.calculate_delta) <= (cards_left//8)+1 and check_if_bot_delta_less_then_other_players_delta:
-            print("Take a card if the card is in the range calculated by the formula (cards_left//8)+1", self.calculate_delta, " <= ", (cards_left//8)+1)
+        if cards_left < 24 and 0 < abs(self.min_delta) <= (cards_left//8)+1 and is_possible_to_collect_series:
+            print("Take a card if the card is in the range calculated by the formula (cards_left//8)+1", self.min_delta, " <= ", (cards_left//8)+1)
             return True
         
         # Take any card if there are no cards available yet and there is more than 5 money on the table.
-        if cards_left < 24 and table_money >= 5 and self.cards == []:
+        if cards_left < 24 and table_card <= 24 and table_money >= 5 and self.cards == []:
             print("Take any card if there are no cards available yet and there is more than 5 money on the table.")
             return True
         
-        if table_card > 20 and table_money / table_card > (0.3+(0.46-0.3)*(table_card-3)/(35-3)):
-            print("Take card if on the table a lot of money")
+        # Take a card if there is a lot of money on the table according to the coefficient snd self.money less than 10
+        # Coefficient K = k_min + (k_max - k_min) * (table_card - x_min) / (x_max - x_min)
+        # k_min = 0.3, k_max = 0.45, x_min = 3, x_max = 35
+        if self.money < 10 and table_card < 28 and table_money / table_card > (0.3+(0.45-0.3)*(table_card-3)/(35-3)):
+            print("Take a card if there is a lot of money on the table according to the coefficient snd self.money less than 10")
             return True
         
         # Take a card if there is no money in the pocket
