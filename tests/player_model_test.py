93f4ffd01228e5020d5beb352c44da91d36f11fe
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2025-01-07 19:08:28 +0200

    Update tests

--- tests/player_model_test.py
+++ tests/player_model_test.py
@@ -13,6 +13,9 @@ with open("json_for_test/game_start.json") as f:
 with open("json_for_test/game_step-02.json") as f:
     data2 = json.load(f)["status"]
 
+with open("json_for_test/game_step-03.json") as f:
+    data3 = json.load(f)["status"]
+
 
 class TestPlayer(unittest.TestCase):
     def setUp(self):
@@ -28,33 +31,33 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player.score, 3)
 
     def test_delta(self):
-        self.assertEqual(self.player.calculate_delta, 0)
+        self.assertEqual(self.player.min_delta, 0)
 
         self.player.update_state(10, [[5, 6]], 4)
-        self.player.calculate_delta = 8
-        self.assertEqual(self.player.calculate_delta, 2)
+        self.player.calculate_deltas = 8
+        self.assertEqual(self.player.min_delta, 2)
 
         self.player.update_state(10, [[5, 6]], 7)
-        self.assertEqual(self.player.calculate_delta, 1)
+        self.assertEqual(self.player.min_delta, 1)
 
         self.player.update_state(10, [[5, 6]], 4)
-        self.assertEqual(self.player.calculate_delta, -1)
+        self.assertEqual(self.player.min_delta, -1)
 
     def test_update_state(self):
         self.player.update_state(10, [[5]], 7)
         self.assertEqual(self.player.money, 10)
         self.assertEqual(self.player.cards, [[5]])
-        self.assertEqual(self.player.calculate_delta, 2)
+        self.assertEqual(self.player.min_delta, 2)
         
         self.player.update_state(5, [[5, 6], [12]], 9)
         self.assertEqual(self.player.money, 5)
         self.assertEqual(self.player.cards, [[5, 6], [12]])
-        self.assertEqual(self.player.calculate_delta, 3)
+        self.assertEqual(self.player.min_delta, 3)
 
         self.player.update_state(5, [[5, 6], [12]], 10)
         self.assertEqual(self.player.money, 5)
         self.assertEqual(self.player.cards, [[5, 6], [12]])
-        self.assertEqual(self.player.calculate_delta, -2)
+        self.assertEqual(self.player.min_delta, -2)
 
 
 class TestBotPlayer(unittest.TestCase):
@@ -65,44 +68,59 @@ class TestBotPlayer(unittest.TestCase):
         for player in data2["players"]:
             if player["name"] != "SergeiVasilyev":
                 self.players.append(Player.from_json(player))
+    
+
+    def _update_players_state(self, table_card: int):
+        for player in self.players + [self.bot_player]:
+            player.update_state(player.money, player.cards, table_card)
 
 
-    def test_compare_deltas(self):
-        self.bot_player.calculate_delta = 19
+    def test_is_possible_to_collect_series(self):
+        print("-------test_is_possible_to_collect_series-------")
+        self._update_players_state(19)
         # If BotPlayer has cards [16, 17] and on the table card 19 (delta = 2) and someone has card 18 (delta = 1), then BotPlayer should bet
-        self.assertEqual(self.bot_player.compare_deltas([3, 5, 3]), True)
-        self.assertEqual(self.bot_player.compare_deltas([1, 5, 3]), False)
+        self.assertEqual(self.bot_player.is_possible_to_collect_series(self.players), False)
+        self._update_players_state(14)
+        self.assertEqual(self.bot_player.is_possible_to_collect_series(self.players), True)
 
 
     def test_decide(self):
-        self.bot_player.calculate_delta = card = 24
-        self.assertEqual(self.bot_player.decide(card, 4, 24, [0, 0, 0], [11, 11, 11]), True)
-        self.assertEqual(self.bot_player.decide(card, 1, 24, [0, 0, 0], [11, 11, 11]), False)
+        print("-------test_decide-------")
+        table_card = 24
+        self._update_players_state(table_card)
+        self.assertEqual(self.bot_player.decide(table_card, 4, 24, self.players), True) # Take if the first move and enough money
+        self.assertEqual(self.bot_player.decide(table_card, 1, 24, self.players), False) # Not enough money on the table
 
-        self.bot_player.calculate_delta = card = 8
-        self.assertEqual(self.bot_player.decide(card, 2, 24, [2, 5, 3], [11, 11, 11]), True)
+        table_card = 8
+        self._update_players_state(table_card)
+        self.assertEqual(self.bot_player.decide(table_card, 2, 24, self.players), True) # If first card is less than 15 and table money is more than 1
 
-        self.bot_player.calculate_delta = card = 14
-        self.assertEqual(self.bot_player.decide(card, 0, 20, [2, -1, 3], [9, 9, 9]), True) # One of the players has delta abs(1)
-        self.assertEqual(self.bot_player.decide(card, 0, 20, [2, 5, 3], [9, 0, 9]), True) # One of the players has no money
+        table_card = 5
+        self._update_players_state(table_card)
+        self.assertEqual(self.bot_player.decide(table_card, 0, 20, self.players), True) # One of the players has delta abs(1)
+        table_card = 14
+        self._update_players_state(table_card)
+        self.assertEqual(self.bot_player.decide(table_card, 3, 20, self.players), True) # One of the players has no money and there is 3 coins on the table
         
-        self.bot_player.calculate_delta = card = 8
+        table_card = 8
+        self._update_players_state(table_card)
         self.bot_player.money = 3
-        self.assertEqual(self.bot_player.decide(card, 5, 20, [2, 5, 3], [9, 9, 9]), True) # If BotPlayer has less than 4 coins in his pocket
+        self.assertEqual(self.bot_player.decide(table_card, 5, 20, self.players), True) # Take a card less than 10 if BotPlayer has less than 4 coins in pocket
 
-        self.bot_player.calculate_delta = card = 14
+        table_card = 14
+        self._update_players_state(table_card)
         self.bot_player.money = 8
-        self.assertEqual(self.bot_player.decide(card, 5, 20, [2, 5, 3], [9, 9, 9]), True) # Take a card if the card is in the range calculated by the formula (cards_left//8)+1
+        self.assertEqual(self.bot_player.decide(table_card, 5, 20, self.players), True) # Take a card if the card is in the range 
 
         self.bot_player.cards = []
-        self.bot_player.calculate_delta = card = 30
-        self.assertEqual(self.bot_player.decide(card, 3, 20, [4, 5, 3], [9, 9, 9]), False)
+        table_card = 29
+        self._update_players_state(table_card)
+        self.assertEqual(self.bot_player.decide(table_card, 3, 20, self.players), False) # If no cards and table card is over 24
 
         self.bot_player.cards = [[10]]
-        self.bot_player.calculate_delta = card = 28
-        self.assertEqual(self.bot_player.decide(card, 13, 20, [4, 5, 3], [9, 9, 9]), True)
-
-
+        table_card = 27
+        self._update_players_state(table_card)
+        self.assertEqual(self.bot_player.decide(table_card, 12, 20, self.players), True)
 
 
 
